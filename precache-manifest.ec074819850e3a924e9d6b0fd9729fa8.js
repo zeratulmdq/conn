b67640b161724ef4927fe364f4fa14699807f8bd
Martin Benvenuti <martinb@mural.co>
2020-09-02 16:56:41 -0300

    Updates

---
+++ precache-manifest.ec074819850e3a924e9d6b0fd9729fa8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a5372dae38aa7c407589bc4d1c101246",
+    "url": "/conn/index.html"
+  },
+  {
+    "revision": "152aec213a25aae60e98",
+    "url": "/conn/static/css/main.413825cb.chunk.css"
+  },
+  {
+    "revision": "d053fe2d69cde258a8c5",
+    "url": "/conn/static/js/2.a58c446a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/conn/static/js/2.a58c446a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "152aec213a25aae60e98",
+    "url": "/conn/static/js/main.fcbef36d.chunk.js"
+  },
+  {
+    "revision": "d2ab0ee1c3b1bb1e6f62",
+    "url": "/conn/static/js/runtime-main.360de52e.js"
+  }
+]);
